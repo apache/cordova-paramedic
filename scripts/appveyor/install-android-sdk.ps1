36418da4898525e0174b2f270d749e4630a9f1ae
Jan Piotrowski <piotrowski+github@gmail.com>
2018-10-02 18:51:50 +0200

    Update install-android-sdk.ps1

--- scripts/appveyor/install-android-sdk.ps1
+++ scripts/appveyor/install-android-sdk.ps1
@@ -3,31 +3,36 @@ if(-not (Test-Path $AndroidToolPath)) {
     $AndroidToolPath = "$env:localappdata\Android\android-sdk\tools\android.bat"
 }
 
-Function Get-AndroidSDKs() { 
-    $output = & $AndroidToolPath list sdk --all 
-    $sdks = $output |% { 
-        if ($_ -match '(?<index>\d+)- (?<sdk>.+), revision (?<revision>[\d\.]+)') { 
-            $sdk = New-Object PSObject 
-            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Index -Value $Matches.index 
-            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Name -Value $Matches.sdk 
-            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Revision -Value $Matches.revision 
-            $sdk 
-        } 
-    } 
-    $sdks 
+Function Get-AllAndroidSDKs() {
+    $output = & $AndroidToolPath list sdk --all
+    $sdks = $output |% {
+        if ($_ -match '(?<index>\d+)- (?<sdk>.+), revision (?<revision>[\d\.]+)') {
+            $sdk = New-Object PSObject
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Index -Value $Matches.index
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Name -Value $Matches.sdk
+            Add-Member -InputObject $sdk -MemberType NoteProperty -Name Revision -Value $Matches.revision
+            $sdk
+        }
+    }
+    $sdks
 }
 
-Function Install-AndroidSDK() { 
-    [CmdletBinding()] 
-    Param( 
-        [Parameter(Mandatory=$true, Position=0)] 
-        [PSObject[]]$sdks 
+Function Execute-AndroidSDKInstall() {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [PSObject[]]$sdks
     )
 
-    $sdkIndexes = $sdks |% { $_.Index } 
-    $sdkIndexArgument = [string]::Join(',', $sdkIndexes) 
-    Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument 
+    $sdkIndexes = $sdks |% { $_.Index }
+    $sdkIndexArgument = [string]::Join(',', $sdkIndexes)
+    Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument
 }
 
-$sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 15*' -or $_.name -like 'google apis*api 15' } 
-Install-AndroidSDK -sdks $sdks 
+Function Install-AndroidSDK
+{
+    param([string]$Level)
+
+    $sdks = Get-AllAndroidSDKs |? { $_.name -like "sdk platform*API $Level*" -or $_.name -like "google apis*api $Level" }
+    Execute-AndroidSDKInstall -sdks $sdks
+}
