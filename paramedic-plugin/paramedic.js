c9aea1045eaeb41c90eb4d731523c6e5798ed78b
Erisu <erisu@apache.org>
2026-03-27 11:21:33 +0900

    chore: license cleanup

--- paramedic-plugin/paramedic.js
+++ paramedic-plugin/paramedic.js
@@ -1,5 +1,5 @@
 /* global window, cordova, jasmine, XMLHttpRequest */
-/**
+/*
     Licensed to the Apache Software Foundation (ASF) under one
     or more contributor license agreements.  See the NOTICE file
     distributed with this work for additional information
